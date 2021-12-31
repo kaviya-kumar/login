295c0f61d2b8e8de00ce5d268794264f0f92db71
kaviya-kumar <kavya.kumar9@gmail.com>
2021-12-31 21:09:25 +0530

    added success pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
-import Success from "./Succesful";
 import { useHistory } from "react-router-dom";
 
 export default function Login() {
